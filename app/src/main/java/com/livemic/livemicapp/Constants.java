345cb5d960f684d9e0b495505769e28af3784535
Prayansh <prayansh@gmail.com>
2018-01-14 01:59:39 -0800

    peer-2-peer impl

--- app/src/main/java/com/livemic/livemicapp/Constants.java
+++ app/src/main/java/com/livemic/livemicapp/Constants.java
@@ -1,13 +1,39 @@
 package com.livemic.livemicapp;
 
-/** Dump any constants in here... */
+/**
+ * Dump any constants in here...
+ */
 public class Constants {
-  public static final String TAG = "LiveMic";
 
-  public static final double UI_AUDIO_SCALE = 1.0; // Lower = zoom in sample UI.
+    public static final String TAG = "LiveMic";
 
-  public static int SAMPLE_RATE_LOCAL_HZ = 32000;
-  public static int SAMPLE_RATE_NETWORK_HZ = 16000;
+    public static final double UI_AUDIO_SCALE = 1.0; // Lower = zoom in sample UI.
 
-  public static int REWRITE_CAPACITY = 8128;
+    public static int SAMPLE_RATE_LOCAL_HZ = 32000;
+    public static int SAMPLE_RATE_NETWORK_HZ = 16000;
+
+    public static int REWRITE_CAPACITY = 8128;
+    public static String PACKAGE_NAME = Constants.class.getPackage().getName();
+
+    // Message
+    public static final int MSG_NULL = 0;
+    public static final int MSG_STARTSERVER = 1001;
+    public static final int MSG_STARTCLIENT = 1002;
+    public static final int MSG_CONNECT = 1003;
+    public static final int MSG_DISCONNECT = 1004;   // p2p disconnect
+    public static final int MSG_PUSHOUT_DATA = 1005;
+    public static final int MSG_NEW_CLIENT = 1006;
+    public static final int MSG_FINISH_CONNECT = 1007;
+    public static final int MSG_PULLIN_DATA = 1008;
+    public static final int MSG_REGISTER_ACTIVITY = 1009;
+
+    public static final int MSG_SELECT_ERROR = 2001;
+    public static final int MSG_BROKEN_CONN = 2002;  // network disconnect
+
+    public static final int MSG_SIZE = 50;    // the lastest 50 messages
+    public static final String MSG_SENDER = "sender";
+    public static final String MSG_TIME = "time";
+    public static final String MSG_CONTENT = "msg";
+
+    public static String KEY_DATA = "DATA";
 }
---
+++ app/src/main/java/com/livemic/livemicapp/model/MessageObject.java
@@ -0,0 +1,55 @@
+package com.livemic.livemicapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+/**
+ * Created by Prayansh on 2018-01-13.
+ */
+
+public class MessageObject implements Parcelable, Serializable {
+    private byte[] audioData;
+    public static final String mDel = "^&^";
+
+    private MessageObject() {
+        audioData = null;
+    }
+
+    public MessageObject(byte[] audioData) {
+        this.audioData = audioData;
+    }
+
+    public MessageObject(Parcel in) {
+        readFromParcel(in);
+    }
+
+
+    @Override public int describeContents() {
+        return 0;
+    }
+
+    public void readFromParcel(Parcel in) {
+        in.readByteArray(audioData);
+    }
+
+    @Override public void writeToParcel(Parcel dest, int flags) {
+        if (audioData != null) {
+            dest.writeByteArray(audioData);
+        }
+    }
+
+    public static final Creator<MessageObject> CREATOR = new Creator<MessageObject>() {
+        @Override
+        public MessageObject createFromParcel(Parcel in) {
+            return new MessageObject(in);
+        }
+
+        @Override
+        public MessageObject[] newArray(int size) {
+            return new MessageObject[size];
+        }
+    };
+
+}
